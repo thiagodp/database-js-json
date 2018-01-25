fd84deccacbdc4a372a8ff092fbd6a27322063a1
Thiago <thiago-dp@bol.com.br>
2018-01-25 00:31:16 -0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # database-js-json
-Database-js driver for JSON files
+
+A [database-js](https://github.com/mlaanderson/database-js) driver for JSON files.
+
+## About
+
+This is a wrapper around the [jl-sql-api](https://github.com/avz/node-jl-sql-api), intended to be used with [database-js](https://github.com/mlaanderson/database-js) for handling JSON files.
+
+## Install
+
+```shell
+npm install database-js-json
+```
+
+## Basic Usage
+
+```javascript
+var Connection = require( 'database-js2' ).Connection;
+
+(async () => {
+    const connection = new Database( 'json:///test.json' );
+    
+    try {
+        let statement = await connection.prepareStatement("SELECT * WHERE user_name = ?");
+        let rows = await statement.query('not_so_secret_user');
+        console.log(rows);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await connection.close();
+    }
+} )();
+```
+
+### Options
+
+Options from [jl-sql-api](https://github.com/avz/node-jl-sql-api) can be passed as arguments to the database connection, in the format of a URL.
+
+Example: `{ tmpDir: "/path/to/dir" }`
+```javascript
+const connection = new Database( 'json:///test.json?tmpDir=path/to/dir' );
+```
+
+When an option that belongs to a group is informed, it must have a dot.
+
+Example: `{ tmpDir: "/path/to/dir", sortOptions: { inMemoryBufferSize: 32000 } }`
+```javascript
+const connection = new Database( 'json:///test.json?tmpDir=path/to/dir&sortOptions.inMemoryBufferSize=32000' );
+```
+
+## License
+
+[MIT](https://github.com/thiagodp/database-js-json/blob/master/LICENSE) (c) [thiagodp](https://github.com/thiagodp)
---
+++ index.js
@@ -0,0 +1,166 @@
+var JlSqlApi = require( 'jl-sql-api' );
+var fs = require( 'fs' );
+
+/**
+ * Database-js driver for JSON files.
+ * 
+ * @see https://github.com/mlaanderson/database-js
+ */
+class JsonDriver {
+
+    /**
+     * Constructor
+     * 
+     * @param {string} filename File name
+     * @param {object} options Options. Defaults to { charset: 'utf-8' }.
+     * 
+     * @see https://github.com/avz/node-jl-sql-api#options-object for more options.
+     */
+    constructor( filename, options ) {
+
+        this._api = new JlSqlApi();
+
+        this._filename = filename;
+
+        this._options = Object.assign( {
+            charset: 'utf-8'
+        }, options );
+
+        this._data = null;
+    }
+
+    /**
+     * Loads the file and returns a promise to its content.
+     * 
+     * @returns {Promise}
+     */
+    _loadFile() {
+        return new Promise( function ( resolve, reject ) {
+            fs.readFile( this._filename, this._options.charset, function ( err, data ) {
+                if ( err ) {
+                    return reject( err );
+                }
+                try {
+                    return resolve( JSON.parse( data ) );
+                } catch ( e ) {
+                    return reject( e );
+                }
+            } );
+        } );
+    }
+
+    /**
+     * Saves the file and returns a promise with no data.
+     * 
+     * @returns {Promise}
+     */    
+    _saveFile() {
+        return new Promise( function ( resolve, reject ) {
+            fs.writeFile( this._filename, this._data || [], this._options.charset, function ( err ) {
+                if ( err ) {
+                    return reject( err );
+                }
+                return resolve();
+            } );
+        } );        
+    }
+
+    /**
+     * Returns a promise to the file content.
+     * 
+     * @param {boolean} reloadFile Whether is desired to reload the file. Defaults to false.
+     * @returns {Promise}
+     */
+    data( reloadFile ) {
+        if ( this._data && ! reloadFile ) {
+            return Promise.resolve( this._data );
+        }
+        return this._loadFile().then( function ( data ) {
+            this._data = data;
+            return data;
+        } );
+    }
+
+    /**
+     * Executes the given query and returns a promise to the resulting data.
+     * 
+     * @param {string} sql Query to execute.
+     * @returns {Promise}
+     */
+    query( sql ) {
+        return new Promise( function( resolve, reject ) {
+
+            var queryData = function( data ) {
+                api.query( sql )
+                    .fromArrayOfObjects( Array.isArray( data ) ? data : [ data ] )
+                    .toArrayOfObjects( function( rows ) {
+                        resolve( rows );
+                    } );
+            };
+
+            this.data()
+                .then( queryData )
+                .catch( function ( err ) { reject( err ); } );
+        } );
+    }
+
+    /**
+     * Executes the given command and returns a promise with the resulting data.
+     * The file is saved after the command execution, whether successful.
+     * 
+     * @param {string} sql Command to execute.
+     * @returns {Promise}
+     */    
+    execute( sql ) {
+
+        return new Promise( function( resolve, reject ) {
+
+            this.query( sql )
+                .then( function keepResults( rows ) {
+                    this._data = rows; // Keep the last content
+                    return rows;
+                } )
+                .then( function saveToFile( rows ) {
+                    return this._saveFile()
+                        .then( function ( data ) { resolve( data ); } )
+                        .catch( function ( err ) { reject( err ); } );
+                } )
+                .catch( function ( err ) { reject( err ); } );
+        } );
+    }
+
+}
+
+module.exports = {
+    open: function( connection ) {
+
+        var options = {};
+        if ( connection.Parameters ) {
+            connection.Parameters.split( '&' ).map( function ( s ) {
+                
+                var separated = s.split( '=' );
+                var key = separated[ 0 ];
+                var val = separated[ 1 ];
+                var subKey = null;
+
+                // e.g. "sortOptions.bufferSize"
+                if ( key.indexOf( '.' ) >= 0 ) {
+                    var sub = key.split( '.' );
+                    key = sub[ 0 ];
+                    subKey = sub[ 1 ];
+                }
+
+                if ( ! subKey ) {
+                    options[ key ] = val;
+                } else {
+                    if ( ! options[ key ] ) {
+                        options[ key ] = {};
+                    }
+                    options[ key ][ subKey ] = val;
+                }
+            });
+        }
+
+        return new JsonDriver( connection.Database, options );
+    }
+};
