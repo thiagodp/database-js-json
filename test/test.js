e973bf2588e021fa8047d491364528eab8e91ce3
Thiago <thiago-dp@bol.com.br>
2018-01-27 16:29:35 -0200

    test.js changed to pass the driver to connection objects

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 var dbjs = require( 'database-js' );
+var driver = require( '../' );
 var assert = require( 'assert' );
 
 describe( 'database-js-json', function () {
 
     var obj = {
-        driverName: '../../../index', // hack to load the index as the json module of database-js
         database: './test/states.json'
     };
 
     it( 'queries an existing file correctly', function( done ) {
-        var conn = new dbjs.Connection( obj );
+        var conn = new dbjs.Connection( obj, driver );
         var st = conn.prepareStatement( 'SELECT * WHERE state = ?' );
         var search = 'Dakota';
         st.query( search )
@@ -26,7 +26,7 @@ describe( 'database-js-json', function () {
         var obj2 = Object.assign( obj, { database: 'not-found.json' } );
         assert.throws(
             function() {
-                new dbjs.Connection( obj2 );
+                new dbjs.Connection( obj2, driver );
             },
             /not exist/
         );
@@ -34,7 +34,7 @@ describe( 'database-js-json', function () {
 
     it( 'does not throw when an inexisting file is not required', function() {
         var obj2 = Object.assign( obj, { database: 'not-found.json', parameters: 'checkOnConnect=false' } );
-        new dbjs.Connection( obj2 );
+        new dbjs.Connection( obj2, driver );
     } );    
 
 } );
