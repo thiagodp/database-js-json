133b01f621667136bb7a944c74095cf93d18bef9
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2018-04-14 21:22:52 -0300

    Adds new tests

--- test/test.js
+++ test/test.js
@@ -4,17 +4,43 @@ var assert = require( 'assert' );
 
 describe( 'database-js-json', function () {
 
-    var obj = {
-        database: './test/states.json'
+    var users = {
+        database: './test/users.json'
     };
 
     it( 'queries an existing file correctly', function( done ) {
-        var conn = new dbjs.Connection( obj, driver );
-        var st = conn.prepareStatement( 'SELECT * WHERE state = ?' );
-        var search = 'Dakota';
+        var conn = new dbjs.Connection( users, driver );
+        var st = conn.prepareStatement( 'SELECT * WHERE username = ?' );
+        var search = 'alice';
         st.query( search )
             .then( function( data ) {
-                assert.equal( data[ 0 ].state, search );
+                assert.equal( data[ 0 ].username, search );
+                done();
+            } )
+            .catch( function( err ) {
+                done( err );
+            } );
+    } );
+
+    it( 'queries single field', function( done ) {
+        var conn = new dbjs.Connection( users, driver );
+        var st = conn.prepareStatement( 'SELECT age WHERE username = ?' );
+        st.query( 'jack' )
+            .then( function( data ) {
+                assert.equal( data[ 0 ].age, 40 );
+                done();
+            } )
+            .catch( function( err ) {
+                done( err );
+            } );
+    } );
+
+    it( 'queries with value inside the query', function( done ) {
+        var conn = new dbjs.Connection( users, driver );
+        var st = conn.prepareStatement( 'SELECT age WHERE username = \'jack\'' );
+        st.query()
+            .then( function( data ) {
+                assert.equal( data[ 0 ].age, 40 );
                 done();
             } )
             .catch( function( err ) {
@@ -23,7 +49,7 @@ describe( 'database-js-json', function () {
     } );
 
     it( 'throws when an inexisting file is required', function() {
-        var obj2 = Object.assign( obj, { database: 'not-found.json' } );
+        var obj2 = Object.assign( users, { database: 'not-found.json' } );
         assert.throws(
             function() {
                 new dbjs.Connection( obj2, driver );
@@ -33,8 +59,8 @@ describe( 'database-js-json', function () {
     } );
 
     it( 'does not throw when an inexisting file is not required', function() {
-        var obj2 = Object.assign( obj, { database: 'not-found.json', parameters: 'checkOnConnect=false' } );
+        var obj2 = Object.assign( users, { database: 'not-found.json', parameters: 'checkOnConnect=false' } );
         new dbjs.Connection( obj2, driver );
-    } );    
+    } );
 
 } );
